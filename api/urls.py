dbfef1c123340329382e61c5f23da4d1df9514e4
ysnghr <yasinyg@code.edu.az>
2021-08-26 17:24:59 +0400

    simple todo app api

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("signup", views.signup),
+    path("login", views.login),
+
+
+    path('todos/', views.TodoListCreate.as_view(),),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view(),),
+    path('todos/<int:pk>/complete', views.TodoComplete.as_view(),),
+    path('todos/completed', views.TodoCompletedList.as_view(),),
+
+]
--- todowoo/settings.py
+++ todowoo/settings.py
@@ -37,8 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'todo',
+    'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ] 
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
